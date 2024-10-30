d0aaafb54d5c6b95d1f0b7fbf60afcf568ea4651
MCL7D9 <101371148+MCL7D9@users.noreply.github.com>
2024-10-30 14:14:06 +0800

    Add files via upload

---
+++ guessNumber.py
@@ -0,0 +1,12 @@
+import random
+n=random.random()*1000000000%100+1
+a=int(input("Guess a number"))
+while a!=n:
+      if a>n:
+          print("Too big")
+      else:
+        print("Too small")
+      a=int(input("Guess a number"))
+print("You Won!")
+    
+      
